c8448893f51a31cf4561e0200fbd3de4faabb2af
Loverde <w.andrew.loverde@gmail.com>
2023-05-16 18:19:05 -0500

    removed for loops in favor of string manipulation within if statements | verified numeric input for passlength

--- assets/script.js
+++ assets/script.js
@@ -2,58 +2,54 @@
 var generateBtn = document.querySelector("#generate");
 
 // list of password letters ands symbols
-var letters = "qazwsxedcrfvtgbyhnujmikolp".split('');
-var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
-// add quotation mark to symbols array
-symbols.push('"'); 
+var letters = "qazwsxedcrfvtgbyhnujmikolp";
+var symbols = " !#$%&'()*+,\\-\"./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
-function generatePassword(){
+function generatePassword() {
   var newPass = '';
-  var charArray = [];
+  var passPool = '';
   // I am presented with a series of prompts for password criteria
-  var passLength = prompt("How many Characters would you like your password to be?");
+  var passLength = Number(prompt("How many Characters would you like your password to be?"));
   var minLength = passLength < 8;
   var maxLength = passLength > 128;
   // Length of password must be at least 8 characters and no more than 128 characters
-  if ( minLength || maxLength){
-    alert("Passwords must be between 8 and 128 characters long");
-  } else if (!minLength && !maxLength){
-    // User selects which criteria to include in the password
-    // assign confirmed characters into charArray
-    if (confirm("Would you like to use UPPERCASE letters?")){
-      for (var i = 0; i < letters.length; i++){
-        charArray.push(letters[i].toUpperCase())
-      } 
-    } 
-    if (confirm("Would you like to use LOWERCASE letters?")){
-      for (var i = 0; i < letters.length; i++){
-        charArray.push(letters[i])
-      } 
-    }
-    if (confirm("Would you like to use NUMBERS?")){
-      for (var i = 0; i < 9; i++){
-        charArray.push(i.toString())
-      } 
-    }
-    if (confirm("Would you like to use SPECIAL characers?")){
-      for (var i = 0; i < symbols.length; i++){
-        charArray.push(symbols[i])
-      } 
-    }
-    if (charArray.length === 0){
-      alert("Password must contain at least 1 character type. Please try again")
-      // return to shut function down early
-      return
-    }
-
-    for (var i=0; i < passLength; i++){
-      // console.log("charArray: "+ charArray + "\n " + charArray.length +"\n index number: "+ Math.floor(Math.random()*charArray.length) + "\n Random Character: "+ charArray[Math.floor(Math.random()*charArray.length)] + "\n newPass: " + newPass);
-      newPass = newPass.concat(charArray[Math.floor(Math.random()*charArray.length)]);
-    }
-  } else {
+  if (isNaN(passLength)) {
     alert("INVALID INPUT: please type a number bewteen 8 and 128");
+    return
   }
+  if (minLength || maxLength) {
+    alert("Passwords must be between 8 and 128 characters long");
+    return
+  }
+  // User selects which criteria to include in the password
+  // assign confirmed characters into charArray
+  if (confirm("Would you like to use UPPERCASE letters?")) {
+    passPool += letters.toUpperCase();
+  }
+  if (confirm("Would you like to use LOWERCASE letters?")) {
+    passPool += letters;
+  }
+  if (confirm("Would you like to use NUMBERS?")) {
+    passPool += '1234567890';
+  }
+  if (confirm("Would you like to use SPECIAL characers?")) {
+    passPool += symbols;
+  }
+  if (passPool.length === 0) {
+    alert("Password must contain at least 1 character type. Please try again")
+    // return to shutdown function early
+    return
+  }
+
+  for (var i = 0; i < passLength; i++) {
+    var randomChar = function () {
+      return Math.floor(Math.random() * passPool.length);
+    }
+    newPass += passPool[randomChar()];
+  }
+
+
   return newPass;
 }
 
@@ -61,7 +57,7 @@ function generatePassword(){
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
 }
